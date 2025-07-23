bbd085610e423ba29b7ae7b39fbc7120e81af75e
zulaikhamir <zulaikhaashiq.cse@gmail.com>
2025-07-23 18:22:24 +0530

    changed content

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div className="w-64 h-screen bg-blue-900 text-white p-6 space-y-4">
-      <h2 className="text-2xl font-bold mb-8">Change Request</h2>
+      <h2 className="text-2xl font-bold mb-8">Menu</h2>
       <nav className="flex flex-col gap-4">
         {" "}
         <Link to="/request-details" className="hover:text-blue-300">
@@ -16,7 +16,7 @@ const Sidebar = () => {
           Budget
         </Link>
         <Link to="/usermanual" className="hover:text-blue-300">
-          Budget User Manual
+          RID User Manual
         </Link>
       </nav>
     </div>
